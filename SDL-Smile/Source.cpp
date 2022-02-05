5fae62e92c7edcca3e98c6f4e0929c6093a4c8b5
William König <william.konig.e@skola.karlskrona.se>
2022-02-05 17:16:42 +0100

    funkar inte riktigt

--- SDL-Smile/Source.cpp
+++ SDL-Smile/Source.cpp
@@ -40,20 +40,24 @@ int main(int argc, char** args)
     bool quit = false;
 
     SDL_Texture* texture = NULL;
+    SDL_Texture* backgroundTextureRoom0 = NULL;
     SDL_Texture* backgroundTextureRoom1 = NULL;
     SDL_Texture* backgroundTextureRoom2 = NULL;
     SDL_Texture* backgroundTextureRoom3 = NULL;
     SDL_Texture* backgroundTextureRoom4 = NULL;
     SDL_Texture* backgroundTextureRoom5 = NULL;
     SDL_Texture* backgroundTextureRoom6 = NULL;
-    SDL_Texture* backgroundTextureRoom7 = NULL;
+    
+
+    // Bakgrunderna ligger i SDL-Smile
+    SDL_Surface* tempBackgroundRoom0 = IMG_Load("roomStart.png");
     SDL_Surface* tempBackgroundRoom1 = IMG_Load("rum1.png");
     SDL_Surface* tempBackgroundRoom2 = IMG_Load("rum2.png");
     SDL_Surface* tempBackgroundRoom3 = IMG_Load("rum3.png");
     SDL_Surface* tempBackgroundRoom4 = IMG_Load("rum4.png");
     SDL_Surface* tempBackgroundRoom5 = IMG_Load("rum5.png");
     SDL_Surface* tempBackgroundRoom6 = IMG_Load("rum6.png");
-    SDL_Surface* tempBackgroundRoom7 = IMG_Load("rum7.png");
+    
     
     SDL_Surface* temp = IMG_Load("face.png");
     SDL_Texture* hud = NULL;
@@ -78,13 +82,23 @@ int main(int argc, char** args)
 
     //Filling texture with the image using a surface
     texture = SDL_CreateTextureFromSurface(renderer, temp);
+    backgroundTextureRoom0 = SDL_CreateTextureFromSurface(renderer, tempBackgroundRoom0);
     backgroundTextureRoom1 = SDL_CreateTextureFromSurface(renderer, tempBackgroundRoom1);
     backgroundTextureRoom2 = SDL_CreateTextureFromSurface(renderer, tempBackgroundRoom2);
+    backgroundTextureRoom3 = SDL_CreateTextureFromSurface(renderer, tempBackgroundRoom3);
+    backgroundTextureRoom4 = SDL_CreateTextureFromSurface(renderer, tempBackgroundRoom4);
+    backgroundTextureRoom5 = SDL_CreateTextureFromSurface(renderer, tempBackgroundRoom5);
+    backgroundTextureRoom6 = SDL_CreateTextureFromSurface(renderer, tempBackgroundRoom6);
     hud = SDL_CreateTextureFromSurface(renderer, tempHud);
     //Deleting the temporary surface
     SDL_FreeSurface(temp);
+    SDL_FreeSurface(tempBackgroundRoom0);
     SDL_FreeSurface(tempBackgroundRoom1);
     SDL_FreeSurface(tempBackgroundRoom2);
+    SDL_FreeSurface(tempBackgroundRoom3);
+    SDL_FreeSurface(tempBackgroundRoom4);
+    SDL_FreeSurface(tempBackgroundRoom5);
+    SDL_FreeSurface(tempBackgroundRoom6);
     SDL_FreeSurface(tempHud);
     SDL_Rect rectHud;
     rectHud.x = 20;
@@ -101,14 +115,14 @@ int main(int argc, char** args)
     SDL_Rect rectBackground;
     rectBackground.x = 0; //Extreme left of the window
     rectBackground.y = 0; //Very bottom of the window
-    rectBackground.w = 800; //100 pixels width
-    rectBackground.h = 600; //100 pixels height
+    rectBackground.w = 800; //800 pixels width
+    rectBackground.h = 600; //600 pixels height
 
     int smileX = playerRect.x;
     int smileY = playerRect.y;
     int hastX = 0;
     int hastY = 0;
-    int roomNr = 1;
+    int roomNr = 0; // Start game in start room
     bool moved = false;
     bool showKey = false;
     while (!quit)
@@ -144,6 +158,7 @@ int main(int argc, char** args)
                     {
                         hastX = 0;
                         moved = false;
+                        std::cout << "X= " << smileX << "  Y= " << smileY << std::endl;
                     }
                     break;
                 case SDLK_RIGHT:
@@ -151,15 +166,18 @@ int main(int argc, char** args)
                     {
                         hastX = 0;
                         moved = false;
+                        std::cout << "X= " << smileX << "  Y= " << smileY << std::endl;
                     }
                     break;
                 case SDLK_UP:
                     if (hastY < 0)
                         hastY = 0;
+                    std::cout << "X= " << smileX << "  Y= " << smileY << std::endl;
                     break;
                 case SDLK_DOWN:
                     if (hastY > 0)
                         hastY = 0;
+                    std::cout << "X= " << smileX << "  Y= " << smileY << std::endl;
                     break;
                 default:
                     break;
@@ -182,30 +200,57 @@ int main(int argc, char** args)
             smileY += hastY;
         playerRect.x = smileX;
         playerRect.y = smileY;
-        if (hastX == -1 && smileX == 0 && roomNr == 1 && (smileY >= 250 || smileY <= 350))
+
+        //Från start rum till rum 4
+        if (hastX == -1 && smileX == 0 && roomNr == 0 && (smileY >= 250 || smileY <= 350))
+        {
+            if (!moved)
+            {
+                moved = true;
+                roomNr = 4;
+                smileX = 700;
+            }
+        }
+        //Från start rum till rum 2
+        else if (hastX == 1 && smileX == 700 && roomNr == 0 && (smileY >= 250 || smileY <= 350))
         {
             if (!moved)
             {
                 moved = true;
                 roomNr = 2;
-                smileX = 700;
-            }
-        }
-        else if (hastX == 1 && smileX >= 699 && roomNr == 2 && (smileY >= 250 || smileY <= 350))
-        {
-            if (!moved)
-            {
-                moved = true;
-                roomNr = 1;
                 smileX = 0;
+                std::cout << "Går till rum 2 från start";
             }
         }
 
         //Copying the texture on to the window using renderer and rectangle
-        if (roomNr == 1)
+        // Renderar det rum man är i
+        if (roomNr == 0) {
+            SDL_RenderCopy(renderer, backgroundTextureRoom0, NULL, &rectBackground);
+        }
+        else if (roomNr == 1) {
             SDL_RenderCopy(renderer, backgroundTextureRoom1, NULL, &rectBackground);
+        }
         else if (roomNr == 2)
+        {
             SDL_RenderCopy(renderer, backgroundTextureRoom2, NULL, &rectBackground);
+        }
+        else if (roomNr == 3)
+        {
+            SDL_RenderCopy(renderer, backgroundTextureRoom3, NULL, &rectBackground);
+        }
+        else if (roomNr == 4)
+        {
+            SDL_RenderCopy(renderer, backgroundTextureRoom4, NULL, &rectBackground);
+        }
+        else if (roomNr == 5)
+        {
+            SDL_RenderCopy(renderer, backgroundTextureRoom5, NULL, &rectBackground);
+        }
+        else if (roomNr == 6)
+        {
+            SDL_RenderCopy(renderer, backgroundTextureRoom6, NULL, &rectBackground);
+        }
 
         if (roomNr == 1 && intersection(playerRect, rectNyckel))
         {
@@ -226,8 +271,14 @@ int main(int argc, char** args)
 
     //Deleting the texture
     SDL_DestroyTexture(texture);
+    SDL_DestroyTexture(backgroundTextureRoom0);
     SDL_DestroyTexture(backgroundTextureRoom1);
     SDL_DestroyTexture(backgroundTextureRoom2);
+    SDL_DestroyTexture(backgroundTextureRoom3);
+    SDL_DestroyTexture(backgroundTextureRoom4);
+    SDL_DestroyTexture(backgroundTextureRoom5);
+    SDL_DestroyTexture(backgroundTextureRoom6);
+    //SDL_DestroyTexture(backgroundTextureRoom7);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
 
